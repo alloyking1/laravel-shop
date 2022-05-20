c27c6a77d22fc7a5b912b5c57fd312b28c9b6ce5
alloyking1 <alloyking1@gmail.com>
2022-05-20 11:22:47 +0100

    feat:added DB table fields and seeder for Usertable and Category table

--- database/migrations/2022_05_19_114729_create_products_table.php
+++ database/migrations/2022_05_19_114729_create_products_table.php
@@ -15,6 +15,10 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('description');
+            $table->string('image');
+            $table->integer('price');
             $table->timestamps();
         });
     }
