b653d774d03fff0287358af6858c66eb4667aa26
alloyking1 <alloyking1@gmail.com>
2022-05-22 13:55:09 +0100

    feat:logout added

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,11 +4,15 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 
+use Illuminate\Http\Request;
 use App\Http\Requests\RegisterUserRequest;
-use App\Http\Resources\RegisterUserResource;
+use App\Http\Requests\LoginUserRequest;
+use App\Http\Resources\UserResource;
 use Exception;
+// use GuzzleHttp\Psr7\Request;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
@@ -26,8 +30,8 @@ class AuthController extends Controller
             return response()->json([
                 'access_token' => $token,
                 'token_type' => 'Bearer',
-                'user' => new RegisterUserResource($newUser)
-            ], 200);
+                'user' => new UserResource($newUser),
+            ], 201);
         } catch (Exception $e) {
             Log::error($e->getMessage());
             return response()->json([
@@ -36,9 +40,48 @@ class AuthController extends Controller
         }
     }
 
-    public function login()
+    public function login(LoginUserRequest $request)
     {
-        return false;
+        try {
+
+            $authAttempt = Auth::attempt($request->only('email', 'password'));
+            if ($authAttempt) {
+                $user = User::where('email', $request->email)->firstOrFail();
+                $token = $user->createToken('auth_token')->plainTextToken;
+                return response()->json([
+                    'user' => new UserResource($user),
+                    'access_token' => $token,
+                    'token_type' => 'Bearer'
+                ], 200);
+            } else {
+                return response()->json([
+                    'user' => "Invalid login details"
+                ], 401);
+            }
+        } catch (Exception $e) {
+            Log::error($e->getMessage());
+            return response()->json([
+                "error" => $e->getMessage(),
+            ], 500);
+        }
+    }
+
+    public function logout(Request $request)
+    {
+        try {
+            Auth::logout();
+            $request->session()->invalidate();
+            $request->session()->regenerateToken();
+
+            return response()->json([
+                'message' => 'Logged out'
+            ], 200);
+        } catch (Exception $e) {
+            Log::error($e->getMessage());
+            return response()->json([
+                "error" => $e->getMessage(),
+            ], 500);
+        }
     }
 
     public function me()
