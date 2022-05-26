1a1e16feb51148e34d410bd802bf355d3690719f
alloyking1 <alloyking1@gmail.com>
2022-05-26 13:51:29 +0100

    feat:categroy request implemented

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -2,12 +2,22 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Http\Requests\CreateCategoryRequest;
+use Exception;
 
 class CategoryController extends Controller
 {
-    public function create()
+
+    public function create(CreateCategoryRequest $request)
     {
-        return "I got here";
+        try {
+            return response()->json([
+                'message' => "I got here after request"
+            ], 200);
+        } catch (Exception $e) {
+            return response()->json([
+                'message' => $e->getMessage()
+            ], 500);
+        }
     }
 }
---
+++ app/Http/Resources/CategoryResource.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class CategoryResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => (string) $this->id,
+            'name' => (string) $this->name,
+            'description' => (string) $this->description,
+            "image" => (string)$this->image,
+            // "created_at" => (string)$this->created_at, //relationship with product
+        ];
+    }
+}
